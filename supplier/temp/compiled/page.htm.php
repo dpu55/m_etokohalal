772f569fd640fd62776e189858bf4cbdbb032123
dpu55 <51227700+dpu55@users.noreply.github.com>
2020-03-23 16:20:28 +0700

    remove temp folder from git

--- supplier/temp/compiled/page.htm.php
+++
@@ -1,6 +0,0 @@
-<p class="page">
-  <a href="javascript:prev_page()" class="prev"><?php echo $this->_var['lang']['page_prev']; ?></a>
-  <span><?php if ($this->_var['filter']['page'] && $this->_var['filter']['page'] >= 0): ?><?php echo $this->_var['filter']['page']; ?><?php else: ?>0<?php endif; ?>/<?php if ($this->_var['filter']['page_count'] && $this->_var['filter']['page_count'] >= 0): ?><?php echo $this->_var['filter']['page_count']; ?><?php else: ?>0<?php endif; ?></span>
-  <a href="javascript:next_page()"><?php echo $this->_var['lang']['page_next']; ?></a>
-  
-</p>
--- temp/query_caches/sqlcache_config_file_b5aeff496f4e0ab99ae0ef143b295209.php
+++
@@ -1,6 +0,0 @@
-<?php
-$this->mysql_config_cache_file_time = 1582872403;
-$this->timeline = 0;
-$this->timezone = 25200;
-$this->platform = 'OTHER';
-?>
